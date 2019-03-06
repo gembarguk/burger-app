5bd77263807f47b75d81622dcad387a538ee1085
Yulia Gembarzhevskaya <yulia.gembarzhevskaya@ippen-digital.de>
2019-03-06 14:52:30 +0100

    Added a Sidedrawer Toggle Button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import Aux from '../../hoc/Auxilary';
+import React, { Component } from 'react';
+import Auxilary from '../../hoc/Auxilary';
 import classes from  "./Layout.css";
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer></SideDrawer>
-        <div> Toolbar, Sidedrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: false
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( (prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+
+    render() {
+        return (
+            <Auxilary>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer}
+                            closed={this.sideDrawerClosedHandler}></SideDrawer>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxilary>
+        )
+    }
+}
+
+
+
+export default Layout;
