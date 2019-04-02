3c2375387af336e346bacd66e552c7a96504fae0
Yulia Gembarzhevskaya <yulia.gembarzhevskaya@ippen-digital.de>
2019-04-02 17:28:49 +0200

    Adding an Orders Page

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './Order.css';
+
+const order = (props) => (
+    <div className={classes.Order}>
+        <p>Ingredients: Salad(1)</p>
+        <p>Price: <stronng>USD 5.45</stronng></p>
+    </div>
+);
+
+export default order;
