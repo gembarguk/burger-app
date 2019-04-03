bb3e98a168b0699386d0e8028cfd06d6843270eb
Yulia Gembarzhevskaya <yulia.gembarzhevskaya@ippen-digital.de>
2019-04-03 17:01:15 +0200

    creating a acustom dynamic input component

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -5,7 +5,6 @@ import classes from './Order.css';
 const order = (props) => {
     const ingredients = [];
 
-    console.log('ingredients', props.ingredients);
     for (let ingredientName in props.ingredients) {
         ingredients.push({
             name: ingredientName,
