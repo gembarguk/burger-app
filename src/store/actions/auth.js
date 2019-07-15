5564c1bd8ca59bb117d909b38f0e9c2e0cf1ff69
Yulia Gembarzhevskaya <yulia.gembarzhevskaya@ippen-digital.de>
2019-07-15 18:14:45 +0200

    added sign-in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -29,8 +29,12 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         };
-        axios.post("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=\n" +
-            "AIzaSyBuU9YH1sIYDYudTl2oFzXhxK1qHdkniRM", authData)
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBuU9YH1sIYDYudTl2oFzXhxK1qHdkniRM';
+        if (!isSignup) {
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBuU9YH1sIYDYudTl2oFzXhxK1qHdkniRM'
+        }
+
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data))
