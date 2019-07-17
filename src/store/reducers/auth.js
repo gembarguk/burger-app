d52ed0227f08ffb2b84a4ca036dd11eda47bdd04
Yulia Gembarzhevskaya <yulia.gembarzhevskaya@ippen-digital.de>
2019-07-17 16:31:12 +0200

    logging out

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -22,10 +22,24 @@ const authSuccess = (state, action) => {
     })
 };
 
+const authLogout = (state, action) => {
+    return updateObject(state, {
+            token: null,
+            userId: null
+        });
+};
+
 const authFail = (state, action) => {
     return updateObject(state, {
         error: action.error,
         loading: false
+    })
+};
+
+const authLogout = (state, action) => {
+    return updateObject(state, {
+        token: null,
+        userId: null
     })
 };
 
@@ -34,6 +48,7 @@ const reducer = (state = initialState, action) => {
         case actionTypes.AUTH_START: return authStart(state, action);
         case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
         case actionTypes.AUTH_FAIL: return authFail(state, action);
+        case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
         default:
             return state;
     }
