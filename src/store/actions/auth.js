1eb4f618f6a593d02d0d8652db956b385707c3ca
Yulia Gembarzhevskaya <yulia.gembarzhevskaya@ippen-digital.de>
2019-07-22 15:57:06 +0200

    saving auth state in local storage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,6 +8,7 @@ export const authStart = () => {
 };
 
 export const authSuccess = (token, userId) => {
+
     return {
         type: actionTypes.AUTH_SUCCESS,
         idToken: token,
@@ -31,6 +32,9 @@ export const checkAuthTimeout = (expirationTime) => {
 };
 
 export const logout = () => {
+     localStorage.removeItem('token');
+     localStorage.removeItem('expirationDate');
+     localStorage.removeItem('userId');
      return {
         type: actionTypes.AUTH_LOGOUT
      };
@@ -52,6 +56,10 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
@@ -68,3 +76,22 @@ export const setAuthRedirectPath = (path) => {
         path: path
     }
 };
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if (expirationDate > new Date()) {
+                dispatch(logout());
+            } else {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+            }
+        }
+    };
+};
+
