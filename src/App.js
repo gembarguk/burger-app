62eeeae4367e4af249969d6a17f873197b7f923c
Yulia Gembarzhevskaya <yulia.gembarzhevskaya@ippen-digital.de>
2019-10-23 17:14:40 +0200

    added hooks to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
@@ -19,12 +19,12 @@ const asyncAuth = asyncComponent(() => {
     return import('./containers/Auth/Auth');
 });
 
-class App extends Component {
-    componentDidMount() {
-        this.props.onTryAutoSignup();
-    }
+const app = props => {
+    useEffect(() => {
+        props.onTryAutoSignup();
+    }, []);
 
-    render() {
+
         let routes = (
             <Switch>
                 <Route path="/auth" component={asyncAuth} />
@@ -33,7 +33,7 @@ class App extends Component {
             </Switch>
         );
 
-        if (this.props.isAuthenticated) {
+        if (props.isAuthenticated) {
             routes = (
                 <Switch>
                     <Route path="/checkout" component={asyncCheckout} />
@@ -53,7 +53,7 @@ class App extends Component {
                 </Layout>
             </div>
         );
-    }
+
 }
 
 const mapStateToProps = state => {
@@ -68,4 +68,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(app))
